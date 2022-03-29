0ebcd4aa814c6f081584be04d08f38c9e2bda94b
KhangNguyen993 <99489107+KhangNguyen993@users.noreply.github.com>
2022-03-29 13:55:00 -0500

    Initial commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class Student {
+    private int id;
+    private String name;
+    private List<Phone> phones;
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phones=" + phones +
+                ", address=" + address +
+                '}';
+    }
+}
